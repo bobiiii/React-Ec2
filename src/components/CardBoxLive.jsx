d34ec6bffdd505e9de36556a0746a66dd5d9beff
bobiiii <52751757+bobiiii@users.noreply.github.com>
2023-06-14 04:54:49 -0700

    14 jun

--- src/components/CardBoxLive.jsx
+++ src/components/CardBoxLive.jsx
@@ -1,12 +1,23 @@
 // import React from 'react'
 import { slide1 } from "../utils/data"
+import wallHerb from "../images/walHerb.png"
+import rewind from "../images/reqind.png"
+import pause from "../images/pause.png"
+import LiveQMark from "../images/LiveQMark.png"
+import Live from "../images/Live.png"
 function CardBoxLive() {
   return (
         < div className="live-box d-flex flex-column align-items-center ">
-        <div className="pt-2"
-        
-        >
-            <h4>walHerb</h4>
+        <div className="d-flex justify-content-between w-100 pt-2 px-3 ">
+            <div className="ps-2">
+              <img className="mx-1" src={wallHerb} alt="wallHerb"/>
+              <img className="mx-1" src={Live} alt="wallHerb"/>
+              <img src={LiveQMark} alt="wallHerb"/>
+            </div>
+            <div className="">
+            <img className=" mx-1 " src={pause} alt="wallHerb"/>
+            <img src={rewind} alt="wallHerb"/>
+            </div>
         </div>
           <div  className="carousel live-box carousel-dark slide" data-bs-interval="false" data-bs-ride="carousel">
             <div className="carousel-inner">
@@ -14,11 +25,11 @@ function CardBoxLive() {
                 <div className="card-wrapper  container-fluid d-flex  ">
                   {slide1.map((item, index) => {
                     return (<div key={index} className="mt-2 live-cards   ">
-                      <div className="card h-75 w-75 pt-2 pb-3  mx-auto    card-cont" key={index} >
-                        <img src={item.img} className="align-self-center h-75" alt="..." />
-                        <div className="card-body  d-flex flex-column justify-content-between">
+                      <div className="card d-flex justify-content-center  mx-3 card-cont" key={index} >
+                        <img src={item.img} className="align-self-center h-50 pt-1" alt="..." />
+                        <div className="card-body px-2 py-1    ">
                           <h5 className="card-title fs-cs-1  ">{item.title}</h5>
-                          <div className="d-flex">
+                          <div className="d-flex ">
                             <div>
                             <i className="bi bi-star"></i>
                             <i className="bi bi-star"></i>
@@ -26,15 +37,15 @@ function CardBoxLive() {
                             <i className="bi bi-star"></i></div>
                             <div><span> &nbsp; {item.rating}</span></div>
                           </div>
-                          <div className="d-flex">
+                          <div className="d-flex ">
                             
                             
-                            <h6 className="fs-cs-2 py-1 ">{item.newPrice}</h6>
+                            <h6 className="fs-cs-2 py-1 mb-0 justify-content-center ">{item.newPrice}</h6>
                           </div>
     
                         </div>
                       </div>
-                      <h6 className="text-center pt-2"> Country</h6>
+                      <h6 className="text-center "> Country</h6>
     
                     </div>)
                   })}
